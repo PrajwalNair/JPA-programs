cf9d788addfe203a3d9175d3d9a0ab2b12d60da6
unknown <you@example.com>
2022-11-02 10:46:42 +0530

    JPA save method

--- krawardees/src/com/xworkz/krawardees/KRAwardeesDisplay.java
+++ krawardees/src/com/xworkz/krawardees/KRAwardeesDisplay.java
@@ -7,9 +7,15 @@ public class KRAwardeesDisplay {
 
 	public static void main(String[] args) {
 		KRAwardeesDTO dto = new KRAwardeesDTO(1, "punit rajkumar", 2022, "film", false, 50, "Bangalore");
+		KRAwardeesDTO dto1 = new KRAwardeesDTO(2, "kuvempu", 1992, "poet", false, 89, "mysore");
+		KRAwardeesDTO dto2 = new KRAwardeesDTO(3, "Dr. Rajkumar", 1992, "actor", false, 76, "Bangalore");
+		KRAwardeesDTO dto3 = new KRAwardeesDTO(4, "Bhimsen jhoshi", 2005, "hindustani classical vocalist", false, 88, "gadag");
 
 		KRAwardeesServiceImpl service = new KRAwardeesServiceImpl();
 		service.validateAndSave(dto);
+		service.validateAndSave(dto1);
+		service.validateAndSave(dto2);
+		service.validateAndSave(dto3);
 	}
 
 }
--- krawardees/src/com/xworkz/krawardees/repository/KRAwardeesRepositoryImpl.java
+++ krawardees/src/com/xworkz/krawardees/repository/KRAwardeesRepositoryImpl.java
@@ -10,18 +10,22 @@ import com.xworkz.krawardees.dto.KRAwardeesDTO;
 
 public class KRAwardeesRepositoryImpl implements KRAwardeesRepository {
 
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.xworkz");
+	EntityManager manager = null;
 	@Override
 	public boolean save(KRAwardeesDTO dto) {
 		try {
-			EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.xworkz");
-			EntityManager manager = factory.createEntityManager();
+			
+			manager = factory.createEntityManager();
 			EntityTransaction transaction = manager.getTransaction();
 			transaction.begin();
 			manager.persist(dto);
 			transaction.commit();
-			manager.close();
+			
 		} catch (PersistenceException e) {
 			e.printStackTrace();
+		}finally {
+			manager.close();
 		}
 		return true;
 	}
